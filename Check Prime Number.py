dfec6280fd91998842330a9b4246f1b662284029
sonikakourav <72182485+sonikakourav@users.noreply.github.com>
2022-10-15 16:31:09 +0530

    Update Check Prime Number.py

--- Check Prime Number.py
+++ Check Prime Number.py
@@ -13,6 +13,8 @@ if num > 1:
     # check for factors
     for i in range(2, num):
         if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
             # break out of loop
             break
 
